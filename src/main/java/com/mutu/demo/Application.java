bc9d8beb5132bce25104e401bbfccf98fe9f342f
Mutu <mutu@gmail.com>
2023-04-01 19:58:03 +0800

    health check process

--- src/main/java/com/mutu/demo/Application.java
+++ src/main/java/com/mutu/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
